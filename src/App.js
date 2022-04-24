3ee434df4ff7d3d1f1eba73c60766c0e6e0bb204
nour-alsatari <nouress98@gmail.com>
2022-04-24 23:22:22 +0300

    testing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ function App() {
   return (
     <div className="App">
      <h1>hi</h1>
+     <p> just testing my github repo if it's linked properly through remote</p>
+     <p> and if i need to use git pull origin branchname --allow-unrelated-histories</p>
     </div>
   );
 }
